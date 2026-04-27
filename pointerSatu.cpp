744c14b69e2c4ed76039e143c44e4c4cfff1e05d
Pebribayu0626 <PebriBayuSatriansyah@gmail.com>
2026-04-27 18:22:59 +0700

    model 3

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -14,11 +14,12 @@ int main() {
     mahasiswa mhs{1}; // Object mhs
     mhs.showNim(); // Member Access Operator
 
-    
+
     mahasiswa &refMhs = mhs; // Pointer Reference refMhs
     refMhs.nim = 2; // Member Access Operator
     mhs.showNim();
 
+    
     mahasiswa *pMhs = &mhs; // pointer Dereference pMhs
     pMhs->nim = 3; // Arrow Operator
     pMhs->showNim();
